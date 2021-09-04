aa53100cf8da19fe721477670c46529ea16ba0b7
RKenny23 <manifestatio@live.com>
2021-09-04 11:27:43 -0400

    win state working

--- main.rb
+++ main.rb
@@ -3,12 +3,12 @@ class Hangman
   def initialize
     @letters = ('a'..'z').to_a
     @word = words.sample.chomp
-    @word_teaser = ""
+    @board = ""
     @lives = 7
     @guesses = []
     
     @word.size.times do
-      @word_teaser += "_ "
+      @board += "_ "
     end
   end
 
@@ -20,27 +20,27 @@ class Hangman
     ["baseball", "soccer", "football"]
   end
 
-  def print_teaser(last_guess = nil)
-    update_teaser(last_guess) unless last_guess.nil?
+  def print_board(last_guess = nil)
+    update_board(last_guess) unless last_guess.nil?
     puts "So far, you've tried: #{@guesses.join(', ')}"
-    puts "\n", @word_teaser, "\n \n"
+    puts "\n", @board, "\n"
   end
 
-  def update_teaser(last_guess)
-    new_teaser = @word_teaser.split
+  def update_board(last_guess)
+    new_board = @board.split
 
-    new_teaser.each_with_index do |letter, index|
+    new_board.each_with_index do |letter, index|
       # replace blank values with letter if matches letter in word
       if letter == '_' && @word[index] == last_guess
-        new_teaser[index] = last_guess
+        new_board[index] = last_guess
       end
     end
 
-    @word_teaser = new_teaser.join(' ')
+    @board = new_board.join(' ')
   end
 
   def make_guess
-    if @lives > 0
+    while @lives > 0 && !won?
       puts "Enter a letter:"
       guess = gets.chomp.downcase
       good_guess = @word.include?(guess)
@@ -50,39 +50,58 @@ class Hangman
       elsif good_guess && !@guesses.include?(guess)
         @guesses << guess
         puts "Good guess!"
-        print_teaser(guess)
+        print_board(guess)
         make_guess
       elsif guess.length > 1 || !@letters.include?(guess)
         puts "Invalid guess! Please try again."
-        print_teaser(guess)
+        print_board(guess)
         make_guess
       elsif @guesses.include?(guess)
         puts "You already guessed that letter!"
-        print_teaser(guess)
-        make_guess      
+        print_board(guess)
+        make_guess   
       else
         @lives -= 1
         @guesses << guess
         puts "Sorry! You have #{@lives} guesses left. Try again." unless @lives == 0
-        print_teaser(guess)
+        print_board(guess)
         make_guess
       end
-    else
-      puts "You ran out of guesses. Game over!", "The word was #{@word}."
     end
+
+    if won?
+      puts "Great job! You win!"
+      play_again
+    else
+      puts "You lose!", "The word was #{@word}."
+      play_again
+    end
+
   end
 
-  def begin
-    puts "Hangman by Ryan", "\n"
+  def start
+    puts "\n", "<<<Hangman by Ryan>>>", "\n"
     puts "Your word is #{@word.length} characters long."
     puts "To quit type 'exit'"
-    print_teaser
+    puts "\n", @board, "\n"
     make_guess
   end
 
+  def won?
+    !@board.include?("_")
+  end
 
-    
+  def play_again
+    puts "\n", "Play again? y/n: "
+    choice = gets.chomp.downcase
+    if choice == 'n'
+      exit
+    elsif choice == 'y'
+      initialize
+      start
+    end
+  end     
 end
 
 game = Hangman.new
-game.begin
+game.start
